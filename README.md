e9fbbab3d27629a18a0c76a2f09cd1ba4b2abb90
vsk <kandhanvsk@gmail.com>
2023-10-04 17:06:02 +0530

    Simple-hasher

---
+++ README.md
@@ -0,0 +1,12 @@
+#SimpleHasher
+Hi guys happt to see you here,
+SimpleHasher is all about making your hashing works simpler.You can invoke it to your projects just by importing simple-haser into your code. After importing you can use a method called 
+#hasher(data,type);
+it takes two parameters,first is the string of data or fileName that you want to hash and second one is the type,whether you are hashing the string value or the file.
+1.For string value just pass the string or if it is a file,then pass the fileName as a string.
+2.To mention a type parameter,if it is a string the use the parameter as 'string' if you are hashing the file then mention it as 'file'
+thats it guys feel free to contact me:[LinkedIn](https://www.linkedin.com/in/vskandhan/)
+
+Thank you guysss :))
+
+Happy Codingg......
---
+++ simple-hasher.js
@@ -0,0 +1,38 @@
+const fs=require('fs')
+const crypto=require('crypto')
+
+let stringHasher=(data)=>{
+    let hash=crypto.createHash('sha512')
+    hash.update(data)
+    let hashValue=hash.digest('hex')
+    return hashValue
+    
+}
+let fileHasher=(data)=>{
+    try{
+    let hash=crypto.createHash('sha512')
+    let fileData=fs.readFileSync(data)
+    hash.update(fileData)
+    return hash.digest('hex')
+    }
+    catch(err){
+        console.log('Error Reading and Hashing the File:',err);
+        return;
+    }
+}
+
+
+let hasher=(data,type)=>{
+    if(type==='string'){
+       return stringHasher(data)
+    }
+    else if(type==='file'){
+        return fileHasher(data)
+    }
+    else{
+        console.log('Please pass the right argument to invoke :)');
+
+    }
+
+}
+module.exports=hasher;
